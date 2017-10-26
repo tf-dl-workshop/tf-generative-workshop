ccb47a3d0066c6ef5eaa80563fdd0016c6a1f56a
James S <sorawitj@gmail.com>
2017-10-26 19:13:15 +0700

    clean import

--- gan/gan_fashion_mnist.py
+++ gan/gan_fashion_mnist.py
@@ -1,8 +1,5 @@
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.gridspec as gridspec
 import os
 from utils import *
 
